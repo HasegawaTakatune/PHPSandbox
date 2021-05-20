7790aef16ed2a1cecef482cbe9c41a9c433379aa
Takatune <0ta5274738t4p4r@gmail.com>
2021-05-21 00:06:56 +0900

    Inputフィールドの配列サンプル

---
+++ sandbox6.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Sword</title>
+</head>
+<body>
+
+<h1>妖刀一覧</h1>
+<form action="./sandbox6_tables.php" method="POST" style="display: flex; flex-direction: column;">
+    <input type="text" name="items[]" value="村正（むらまさ）" readonry>
+    <input type="text" name="items[]" value="正宗（まさむね）" readonry>
+    <input type="text" name="items[]" value="宗三左文字（そうざさもんじ）" readonry>
+    <input type="text" name="items[]" value="菊一文字則宗（きくいちもんじのりむね）" readonry>
+    <input type="text" name="items[]" value="童子切安綱（どうじきりやすつな）" readonry>
+    <input type="text" name="items[]" value="鬼丸（おにまる）" readonry>
+    <input type="text" name="items[]" value="三日月宗近（みかづきむねちか）" readonry>
+    <input type="text" name="items[]" value="数珠丸（じゅずまる）" readonry>
+    <input type="text" name="items[]" value="大典太（おおてんた）" readonry>
+    <input type="text" name="items[]" value="歌仙兼定（かせんかねさだ）" readonry>
+    <input type="text" name="items[]" value="蕨手刀（わらびてとう）" readonry>
+    <input type="text" name="items[]" value="塵地螺鈿飾剣（ちりじらでんかざりつるぎ）" readonry>
+    <input type="text" name="items[]" value="青木兼元（あおきかねもと）" readonry>
+    <input type="text" name="items[]" value="天之尾羽張（あめのおはばり）" readonry>
+    <input type="text" name="items[]" value="天羽々斬（あめのはばきり）" readonry>
+    <input type="text" name="items[]" value="今剣（いまのつるぎ）" readonry>
+    <input type="text" name="items[]" value="岩通（いわとおし）" readonry>
+    <input type="text" name="items[]" value="大包平（おおかねひら）" readonry>
+    <input type="submit" value="一覧取得">
+</form>
+
+</body>
---
+++ sandbox6_tables.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+<?php
+$rows = isset($_POST['items']) ? $_POST['items'] : null;
+?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script type="text/javascript" src="techAcademy.js"></script>
+
+    <title>Sword</title>
+</head>
+
+<body>
+
+    <table border="1" id="sword-table">
+        <tr>
+            <th>No.</th>
+            <th>名前（かな）</th>
+        </tr>
+        <?php for($i = 0; $i < count($rows); $i++){ ?>
+            <tr><td><?=($i+1)?></td><td><?=$rows[$i]?></td></tr>
+        <?php } ?>
+    </table>
+
+</body>
